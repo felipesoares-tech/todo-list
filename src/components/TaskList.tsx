381496d82a9fe075987ea04602f4a917d72513d0
Felipe Soares <soares.felipe200@gmail.com>
2023-02-21 15:38:14 -0300

    estilizando lista

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -9,15 +9,22 @@ interface IAppProps {
 const TaskList = ({ taskList }: IAppProps) => {
     return (
         <>
-        {taskList.length > 0 ? (
-            taskList.map((task)=> (
-                <div key={task.id}>
-                    <p>{task.title}</p>
-                </div>
-            ))
-        ): (
-            <p>Não há tarefas cadastradas</p>
-        )}
+            {taskList.length > 0 ? (
+                taskList.map((task) => (
+                    <div key={task.id} className={styles.task}>
+                        <div className={styles.details}>
+                            <h4>{task.title}</h4>
+                            <p>Dificuldade: {task.difficulty}</p>
+                        </div>
+                        <div className={styles.actions}>
+                            <i className='bi bi-pencil'></i>
+                            <i className='bi bi-trash'></i>
+                        </div>
+                    </div>
+                ))
+            ) : (
+                <p>Não há tarefas cadastradas</p>
+            )}
         </>
     )
 };
